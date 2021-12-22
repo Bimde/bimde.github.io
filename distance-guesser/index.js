94cd218513811db4537b54ede1f6229eeb989c86
Bimesh De Silva <b2desilv@edu.uwaterloo.ca>
2021-12-22 18:22:39 -0500

    distance guesser

---
+++ distance-guesser/index.js
@@ -0,0 +1,33 @@
+function latLng(geocoder, addr) {
+    return geocoder
+        .geocode({ address: addr })
+        .then((response) => {
+            if (!response.results[0]) {
+                ans.innerHTML = "Lat/Lng not found for " + loc1;
+                return;
+            }
+            return response.results[0].geometry.location
+        })
+}
+
+async function loadDistance(geocoder, addr1, addr2, ans) {
+    let latlng1 = await latLng(geocoder, addr1);
+    let latlng2 = await latLng(geocoder, addr2);
+    let distance = google.maps.geometry.spherical.computeDistanceBetween(latlng1, latlng2);
+    console.log(distance)
+    ans.innerHTML = "Distance from " + addr1 + " to " + addr2 + " is " + Math.trunc(distance / 1000) + " km!"
+}
+
+function initMap() {
+    console.log("Running init map")
+    const geocoder = new google.maps.Geocoder();
+    document.getElementById("calc-button").onclick = function () {
+        let loc1 = document.getElementById("location-1").value
+        let loc2 = document.getElementById("location-2").value
+        console.log("loc1: " + loc1)
+        console.log("loc2: " + loc2)
+        let ans = document.getElementById("distance-answer")
+        ans.innerHTML = "Loading distance from " + loc1 + " to " + loc2 + "..."
+        loadDistance(geocoder, loc1, loc2, ans)
+    }
+}
